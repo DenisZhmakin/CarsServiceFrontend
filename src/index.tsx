1757f639413c14e630b46313e25fc061601aa59e
DenisZhmakin <den.zhmakin.00@gmail.com>
2024-11-27 12:12:37 +0300

    feat: Добавил React проект.

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+import HomePage from "./pages/HomePage/HomePage.tsx";
+
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <HomePage/>
+        }
+    ]
+)
+
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <RouterProvider router={router}/>
+    </StrictMode>,
+)
